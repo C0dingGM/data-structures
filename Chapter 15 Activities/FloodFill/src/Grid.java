284f3ec8d40d6e3364f6d5b605ced12cfb592aed
C0dingGM <yutianw2020@gmail.com>
2024-09-23 14:44:54 -0500

    I was not buggin

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,67 +1,54 @@
 import java.util.*;
 
-public class Grid
-{
+public class Grid {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
     int fillnum = 1;
     Stack<Pair> cords = new Stack<>();
-   
+
     /**
      * Flood fill, starting with the given row and column.
-    */
-    public void floodfill(int iRow, int iColumn)
-    {
-        
+     */
+    public void floodfill(int iRow, int iColumn) {
+        System.out.println("Start");
         cords.add(new Pair(iRow, iColumn));
         int num = 0;
-        while (!cords.isEmpty()){
+        while (!cords.isEmpty()) {
             Pair cord = cords.pop();
             int row = cord.row;
             int col = cord.col;
 
-            if (row >= 0 && row < 10 && col >= 0 && col < 10 && pixels[row][col] == 0){
+            if (row >= 0 && row < 10 && col >= 0 && col < 10 && pixels[row][col] == 0) {
                 pixels[row][col] = fillnum++;
 
-                
-                
-                
-                cords.push(new Pair(row-1, col));
-                cords.push(new Pair(row, col+1));
-                cords.push(new Pair(row+1, col));
-                cords.push(new Pair(row, col-1));
-                 
-                
-                
-                
-                
+                cords.push(new Pair(row - 1, col));
+                cords.push(new Pair(row, col + 1));
+                cords.push(new Pair(row + 1, col));
+                cords.push(new Pair(row, col - 1));
 
-            } 
+            }
             num++;
             /*
-             for (int i = 0; i < pixels.length; i++){
-                for (int x = 0; x < pixels.length; x ++){
-                    System.out.print(pixels[i][x]+ " ");
-                }
-                System.out.println();
-            }
-            System.out.println();
-            
+             * for (int i = 0; i < pixels.length; i++){
+             * for (int x = 0; x < pixels.length; x ++){
+             * System.out.print(pixels[i][x]+ " ");
+             * }
+             * System.out.println();
+             * }
+             * System.out.println();
+             * 
+             * 
+             * }
+             */
 
         }
-             */
-            
-        
-        }
-       
+
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         String r = "";
-        for (int i = 0; i < SIZE; i++)
-        {
+        for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++)
                 r = r + String.format("%4d", pixels[i][j]);
             r = r + "\n";
--- Chapter 15 Activities/FloodFillQueue/src/Grid.java
+++ Chapter 15 Activities/FloodFillQueue/src/Grid.java
@@ -1,23 +1,19 @@
-. . .
-public class Grid
-{
+//. . .
+public class Grid {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    // . . .
 
     /**
      * Flood fill, starting with the given row and column.
-    */
-    public void floodfill(int row, int column)
-    {
-        . . .
+     */
+    public void floodfill(int row, int column) {
+        // . . .
     }
 
-    public String toString()
-    {
+    public String toString() {
         String r = "";
-        for (int i = 0; i < SIZE; i++)
-        {
+        for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++)
                 r = r + String.format("%3d", pixels[i][j]);
             r = r + "\n";
